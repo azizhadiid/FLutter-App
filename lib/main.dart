eca0e71ad4cffe9947236f4a98a82ff6bca34149
azizhadiid <azizalhadiid88@gmail.com>
2025-07-30 13:54:03 +0700

    Recap

--- lib/main.dart
+++ lib/main.dart
@@ -21,25 +21,45 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Mapp'),
-          centerTitle: true,
-        ), // AppBar
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ), // NavigationDestination
-            NavigationDestination(
-              icon: Icon(Icons.person),
-              label: 'Profile',
-            ), // NavigationDestination
-          ],
-          onDestinationSelected: (int value) {},
-          selectedIndex: 1,
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    // Here
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Mapp'), centerTitle: true),
+      body: currentIndex == 0
+          ? Center(child: Text('1')) // Center
+          : Center(child: Text('2')), // Center
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ), // NavigationDestination
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ), // NavigationDestination
+        ],
+        onDestinationSelected: (int value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        selectedIndex: currentIndex,
       ),
     );
   }
