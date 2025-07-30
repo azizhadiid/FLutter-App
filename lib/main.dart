a3c04efa8d4a87ac20ee00ad5a46f812f772902a
azizhadiid <azizalhadiid88@gmail.com>
2025-07-30 14:25:09 +0700

    Widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-// state
-// material app
-//scaffold
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,46 +25,7 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    // Here
-    return Scaffold(
-      appBar: AppBar(title: Text('Flutter Mapp'), centerTitle: true),
-      body: currentIndex == 0
-          ? Center(child: Text('1')) // Center
-          : Center(child: Text('2')), // Center
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ), // NavigationDestination
-          NavigationDestination(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ), // NavigationDestination
-        ],
-        onDestinationSelected: (int value) {
-          setState(() {
-            currentIndex = value;
-          });
-        },
-        selectedIndex: currentIndex,
-      ),
+      home: WidgetTree(),
     );
   }
 }
---
+++ lib/views/widget_tree.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/views/pages/home_page.dart';
+import 'package:flutter_app/views/pages/profile_page.dart';
+import 'package:flutter_app/widgets/navbar_widgets.dart';
+
+List<Widget> pages = [HomePage(), ProfilePage()];
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Hadezer Mapp'), centerTitle: true),
+      body: pages.elementAt(0),
+      bottomNavigationBar: NavbarWidget(),
+    );
+  }
+}
