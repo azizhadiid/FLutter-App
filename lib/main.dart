5e95971980ba4e42bd1f55258e852787f5bb6cdc
azizhadiid <azizalhadiid88@gmail.com>
2025-08-01 20:55:12 +0700

    Get Started Flutter with Free Code Camp

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/data/constans.dart';
-import 'package:flutter_app/data/notifiers.dart';
-import 'package:flutter_app/views/pages/welcome_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    initThemeMode();
-    super.initState();
-  }
-
-  void initThemeMode() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final bool? repeat = prefs.getBool(KConstants.themeModeKey);
-    isDartkModeNotifier.value = repeat ?? false;
-  }
-
-  int selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: isDartkModeNotifier,
-      builder: (BuildContext context, dynamic isDartkMode, Widget? child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.teal,
-              brightness: isDartkMode ? Brightness.dark : Brightness.light,
-            ),
-          ),
-          home: WelcomePage(),
-        );
-      },
-    );
-  }
 }
