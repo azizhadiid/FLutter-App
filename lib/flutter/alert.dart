1ef77900d8a6309753d3c4ccc3729e41a15821c7
azizhadiid <azizalhadiid88@gmail.com>
2025-08-19 12:04:07 +0700

    Learn Alert

---
+++ lib/flutter/alert.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dialog Example")),
+      body: Center(
+        child: TextButton(
+          onPressed: () => showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('AlertDialog Title'),
+              content: const Text('AlertDialog description'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'Cancel'),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'OK'),
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          ),
+          child: const Text('Show Dialog'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/flutter/button.dart';
+import 'package:flutter_app/flutter/alert.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +14,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData(colorSchemeSeed: const Color.fromARGB(255, 1, 51, 19)),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Page")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const DialogExample()),
+            );
+          },
+          child: const Text("Go to Alert Page"),
+        ),
       ),
-      home: const ButtonApp(),
     );
   }
 }
